67108cdbc81d6cdfbf72e199d153fd4ab6388f13
samuelmarcell21 <samuelmarcell21@gmail.com>
2020-02-18 21:49:17 +0700

    tampilan sb admin belom keload

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,5 +53,7 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-// $route['about'] = 'welcome/about';
-// $route['contact'] = 'welcome/contact';
+$route['about'] = 'welcome/about';
+$route['contact'] = 'welcome/contact';
+
+$route['admin'] = 'admin/overview';
